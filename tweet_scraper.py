6dc8ea6c703bfb62936b5a4910d8179aa29738f9
harshitsoni01 <soniharshit2000@gmail.com>
2020-07-02 16:44:26 +0530

    Add csv folder

--- tweet_scraper.py
+++ tweet_scraper.py
@@ -19,12 +19,12 @@ user2_tweets = query_tweets("@GossipCop" , begindate= begin_date, enddate=end_da
 
 df = pd.DataFrame(b.__dict__ for b in basic_tweets)
 df.drop(df.columns.difference(['username','text']), 1, inplace=True)
-df.to_csv("C:/fake_news_detection/tweet.csv" ,index= False)
+df.to_csv("C:/fake_news_detection/csv_files/tweet.csv" ,index= False)
 
 df1 = pd.DataFrame(u1.__dict__ for u1 in user1_tweets)
 df1.drop(df1.columns.difference(['username','text']), 1, inplace=True)
-df1.to_csv("C:/fake_news_detection/politifact.csv" , index= False)
+df1.to_csv("C:/fake_news_detection/csv_files/politifact.csv" , index= False)
 
 df2 = pd.DataFrame(u2.__dict__ for u2 in user2_tweets)
 df2.drop(df2.columns.difference(['username','text']), 1, inplace=True)
-df2.to_csv("C:/fake_news_detection/gossipcop.csv" , index= False)
+df2.to_csv("C:/fake_news_detection/csv_files/gossipcop.csv" , index= False)
