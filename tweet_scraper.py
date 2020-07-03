d9e38361315484c90626c72ed0e31ec505ffe9cb
harshitsoni01 <soniharshit2000@gmail.com>
2020-07-03 18:07:25 +0530

    changed tweet

--- tweet_scraper.py
+++ tweet_scraper.py
@@ -15,17 +15,17 @@ langs = "english"
 #user2 = GossipCop
 
 basic_tweets = query_tweets("#news" , begindate=begin_date, enddate=end_date , limit=limits, lang=langs)
-# user1_tweets = query_tweets("@PolitiFact" , begindate= begin_date, enddate=end_date, limit=limits, lang=langs)
-# user2_tweets = query_tweets("@GossipCop" , begindate= begin_date, enddate=end_date, limit=limits, lang=langs)
+user1_tweets = query_tweets("@PolitiFact" , begindate= begin_date, enddate=end_date, limit=limits, lang=langs)
+user2_tweets = query_tweets("@GossipCop" , begindate= begin_date, enddate=end_date, limit=limits, lang=langs)
 
 df = pd.DataFrame(b.__dict__ for b in basic_tweets)
 df.drop(df.columns.difference(['username','text']), 1, inplace=True)
 df.to_csv("C:/fake_news_detection/csv_files/tweet.csv", index= False)
 
-# df1 = pd.DataFrame(u1.__dict__ for u1 in user1_tweets)
-# df1.drop(df1.columns.difference(['username','text']), 1, inplace=True)
-# df1.to_csv("C:/fake_news_detection/csv_files/politifact.csv" , index= False)
+df1 = pd.DataFrame(u1.__dict__ for u1 in user1_tweets)
+df1.drop(df1.columns.difference(['username','text']), 1, inplace=True)
+df1.to_csv("C:/fake_news_detection/csv_files/politifact.csv" , index= False)
 
-# df2 = pd.DataFrame(u2.__dict__ for u2 in user2_tweets)
-# df2.drop(df2.columns.difference(['username','text']), 1, inplace=True)
-# df2.to_csv("C:/fake_news_detection/csv_files/gossipcop.csv" , index= False)
+df2 = pd.DataFrame(u2.__dict__ for u2 in user2_tweets)
+df2.drop(df2.columns.difference(['username','text']), 1, inplace=True)
+df2.to_csv("C:/fake_news_detection/csv_files/gossipcop.csv" , index= False)
