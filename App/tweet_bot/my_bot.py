85032ce073db48ce59d1ca040f2d87421ade5ce1
zensajnani <zen@sajnani.com>
2020-07-05 00:01:05 +0530

    Created twitter bot

--- App/tweet_bot/my_bot.py
+++ App/tweet_bot/my_bot.py
@@ -1,5 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[24]:
+
+
 import tweepy
 import time
+import joblib
+
+#Twitter Bot
+
+
+# In[26]:
+
 
 print("This is my bot!")
 
@@ -15,6 +28,12 @@ api = tweepy.API(auth)
 
 FILE_NAME = 'last_seen_id.txt'
 
+pipeline = joblib.load('../pipeline_final.sav')
+
+
+# In[27]:
+
+
 #this to reply to tweets only once 
 #this is to retrieve the last seen id
 def retrieve_last_seen_id(FILE_NAME):
@@ -23,12 +42,31 @@ def retrieve_last_seen_id(FILE_NAME):
     f_read.close()
     return last_seen_id
 
+
+# In[28]:
+
+
 #this is to store last seen id/rewrite it 
 def store_last_seen_id(last_seen_id, FILE_NAME):
     f_write = open(FILE_NAME, 'w')
     f_write.write(str(last_seen_id))
+    
     f_write.close()
     return
+
+
+# In[29]:
+
+
+#Detect if tweet is real or fake news
+def predict_tweet_news(text):
+    pred = pipeline.predict(text)
+    return pred
+
+
+# In[32]:
+
+
 
 def reply_to_tweets():
     print('retrieving and replying to tweets...')
@@ -46,13 +84,19 @@ def reply_to_tweets():
             print('found #news')
             print('responding back...')
             #this is used to reply to the tweets tagged to us
-            api.update_status('@' + mention.user.screen_name +
-                    '#news This could be a fake or a real news!', mention.id)
-    return mentions
+            tweet_received = [mention.full_text]
+            prediction = predict_tweet_news(tweet_received)
+            api.update_status(f'#news @{mention.user.screen_name} This is {prediction}!', mention.id)
 
-def get_tweet_text(mentions):
-    return mentions
 
-while True:
-    reply_to_tweets()
-    time.sleep(2)
+# In[33]:
+
+
+reply_to_tweets()
+
+
+# In[ ]:
+
+
+
+
